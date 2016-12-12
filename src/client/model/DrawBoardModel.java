2a3ef7947d0dc6ba642e5cfb4d03c92ed44c24d2
robinlzl <zlei2@wpi.edu>
2016-12-11 21:01:29 -0500

    V1.1.2 NEW3

--- src/client/model/DrawBoardModel.java
+++ src/client/model/DrawBoardModel.java
@@ -3,23 +3,51 @@ package client.model;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * 
+ * This class provides useful methods to draw 4 * 4 board
+ *
+ */
 public class DrawBoardModel {
 
-
+	/**
+	 * The origin point of the 4 * 4 board
+	 */
 	private Point origin;     // The origin point of the 4 by 4 board
 	//private Point cellLt;
 	
 	private Point point;
+	/**
+	 * an attribute used for debug
+	 */
 	private Point oldSelPoint; 
-	
+	/**
+	 * 
+	 * The size of the 4 * 4 board 
+	 */
 	private int size;
-	private int cellSize;
 	
-	private List<String> selCellList; // store the selected cells
+	/**
+	 * The cell size of the 4 * 4 board
+	 */
+	private int cellSize;
+	/**
+	 *  Stores the selected cells, based on type of string
+	 */
+	private List<String> selCellList; 
+	/**
+	 * Stores the selected cells based on type of cell 
+	 */
 	private List<Cell> selCellList2;
+	
+	/**
+	 * Used to get the letters in the board
+	 */
 	private Board board;
 	
+	/**
+	 * Initialize several points used for drawing board
+	 */
 	public DrawBoardModel() {
 		
 		origin = new Point();
@@ -41,38 +69,60 @@ public class DrawBoardModel {
 		selCellList = new ArrayList<String>();
 		selCellList2 = new ArrayList<Cell>();
 	}
-	
+	/**
+	 * Sets the board 
+	 * @param board
+	 */
 	public void setBoard(Board board) { 
 		
 		this.board = board;
 	}
-	
+	/**
+	 * Returns the board
+	 * @return
+	 */
 	public Board getBoard() {
 		
 		return board;
 	}
-	
+	/**
+	 * Returns the list of cells 
+	 * @return
+	 */
     public List<String> getSelCellList() {
 		
 		return selCellList;
 	}
-    
+    /**
+     * Clears the selected cell list 
+     */
     public void clearSelCellList() {
     	
     	selCellList.clear();
     	selCellList2.clear();
     }
     
+   /**
+    * Returns the size of cell list
+    * @return
+    */
     public int getSelCellListSize(){
     	return selCellList.size();
     }
     
+    /**
+     * Returns the list of cells 
+     * @return
+     */
     public List<Cell> getSelCellList2() {
     	
     	return selCellList2;
     }
-
     
+    /**
+     * Adds the selected point to the cell lists, interact with class with drag control in controller class
+     * @param p
+     */
     public void addDragCellList(Point p) {
     	
     	String s;
@@ -117,7 +167,11 @@ public class DrawBoardModel {
         }   	
         
     }
-	
+	/**
+	 * Returns the horizon line based on the row id 
+	 * @param rowId
+	 * @return
+	 */
 	public Point getHorizenLine(int rowId) {
 		
 		point.x = origin.x;
@@ -125,15 +179,23 @@ public class DrawBoardModel {
 		
 		return point;
 	}
-	
+	/**
+	 * Returns the point of vertical line based on column id 
+	 * @param colId
+	 * @return
+	 */
 	public Point getVerticalLine(int colId) {
 		
 		point.x = origin.x + colId * size / 4;   //4x4
 		point.y = origin.y;
 		return point;
 	}
-	
-	//get cell left top point
+	/**
+	 * Get cell left top point
+	 * @param row
+	 * @param col
+	 * @return
+	 */
 	public Point getCellLt(int row, int col) {
 		
 		point.x = origin.x + col * size / 4 + 1;	
@@ -143,25 +205,43 @@ public class DrawBoardModel {
 	
 	}
 	
+	/**
+	 * Returns a letter in the board based on the given row and column
+	 * @param row
+	 * 		  row 
+	 * @param col 
+	 * 		  column
+	 * @return 
+	 */
 	public String getCellLetter(int row, int col) {
 	   
 		return board.getPositionLetter(row, col);
 		
 	}
 	
-	public int getBoardSize()
-	{
+	/**
+	 * Returns the size of the board
+	 * @return 
+	 */
+	public int getBoardSize() {
 		return size;
 	
 	}
-	
-	public int getCellSize()
-	{
+	/**
+	 * Return the size of cell
+	 * @return
+	 */
+	public int getCellSize() {
+		
 		return cellSize;
 	
 	}
 
-	
+	/**
+	 * Returns the word the user selects
+	 * @param cellList gather the selected cell 
+	 * @return
+	 */
 	public String getWord(List<Cell> cellList) {
 		
 		String s = "";
